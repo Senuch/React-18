72f4bdbfda8a346d029c54e4da17dbcebef14cfc
Senuch <s.uzairtariq@gmail.com>
2024-07-17 21:33:34 +0500

    Added List Group, Button and Alert Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import ListGroup from "./components/ListGroup";
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
-  return <div><ListGroup/></div>
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  return (
+    <div>
+      {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My alert!!</Alert>}
+      <Button onClick={() => setAlertVisibility(true)}>My Button</Button>
+    </div>
+  );
 }
 
 export default App;
